413f89b500d1a10f3c05c91d44967c967c544988
Jayla House <housejayla6115@gmail.com>
2022-11-28 02:16:33 -0500

    Fixed compile errors with anim code
    Rigidbody needed a lowercase B and GetComponent was not spelled right

--- My project/Assets/scripts/animations.cs
+++ My project/Assets/scripts/animations.cs
@@ -4,6 +4,51 @@ using UnityEngine;
 
 public class animations : MonoBehaviour
 {
+    [SerializeField]
+    private float walkSpeed = 0.3f;
 
+    private Animator animator;
+
+    private float xAxis;
+    private Rigidbody2D rb2d;
+    private string currAnim;
+    private bool isAttackPressed;
+    private bool isattacking;
+
+    [SerializeField]
+    private float attackDelay = 0.3f;
+
+    //animation states 
+    const string Player_Idle = "idle_1";
+    const string Player_Swing = "swing_1";
+    const string Player_Walking = "walking";
+
+    void Start(){
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update(){
+        xAxis = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            isAttackPressed = true;
+        }
+    }
+
+    void FixedUpdate(){
+        Vector2 vel = new Vector2(0, rb2d.velocity.y);
+
+        if(xAxis < 0){
+            vel.x = -walkSpeed;
+            transform.localScale = new Vector2(-1, 1);
+        }else if(xAxis > 0 ){
+            vel.x = walkSpeed;
+            transform.localScale = new Vector2(1, 1); 
+        }else{
+            vel.x = 0;
+        }
+    }
    
 }
